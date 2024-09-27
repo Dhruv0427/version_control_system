01297dca8dbbb078637cf0b03efbc9ec878cbe74
Dhruv0427 <b23ee1086@iitj.ac.in>
2024-09-27 06:20:59 +0530

    Add files via upload

---
+++ init.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<filesystem>
+#include "my_vcs.h"
+
+void init(){
+if(!std::filesystem::exists(".vcs")) {
+    std::filesystem::create_directory(".vcs");
+    std::filesystem::create_directory(".vcs/objects/");
+    std::filesystem::create_directory(".vcs/staging");
+    
+    std::cout << "Directory structure '.vcs' created in the directory: " 
+              << std::filesystem::current_path() << std::endl;
+    std::cout << "---------------------------------------------------------------------------------------------------" << std::endl;
+}
+  else{
+    std::cout<<"'.vcs' file already exist"<<std::endl;
+  }
+
+  return;
+}
