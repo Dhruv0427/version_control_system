6a3e7c80256522e2b4372a1e9762b0bd5670976f
Dhruv0427 <b23ee1086@iitj.ac.in>
2024-10-04 15:57:06 +0530

    Update my_vcs.h

--- my_vcs.h
+++ my_vcs.h
@@ -5,7 +5,14 @@
 
 void init();
 void add(const std::string& filename);
+std::string generate_commit_hash(const std::vector<std::string>& file_hashes, const std::string& message, const std::string& prev_commit);
+std::string calculate_sha1_hash(const std::string& input);
 void commit(const std::string& message);
 void log();
+
+void restore_file(const std::string& filename);
+void restore_file_version(const std::string& filename, std::size_t version_number);
+void list_file_versions(const std::string& filename);
+std::pair<std::string, std::string> split_versioned_filename(const std::string& versioned_file);
 
 #endif
