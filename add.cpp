a195ecf5d98b2aec65b797fa0501534986c55243
Dhruv0427 <b23ee1086@iitj.ac.in>
2024-10-14 15:51:37 +0530

    Updated add.cpp to add file in staging area

--- add.cpp
+++ add.cpp
@@ -1,24 +1,53 @@
-#include<filesystem>
-#include<iostream>
-#include<cstring>
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include "my_vcs.h"
 
 const std::string STAGING_DIR = ".vcs/staging";
+const std::string BLOBS_DIR = ".vcs/blobs";
+const std::string INDEX_FILE = STAGING_DIR + "/index.txt";  
 
-void add(const std::string& filename){
 
-  if(!std::filesystem::exists(STAGING_DIR)){
-      std::filesystem::create_directory(STAGING_DIR);
-   }
-   std::filesystem::path stagefile = STAGING_DIR + "/" + filename;
 
-   try{
-     std::filesystem::copy_file(filename,stagefile,std::filesystem::copy_options::overwrite_existing);
-     std::cout<<"File "<<filename<<" is staged "<<std::endl;
-   }
-   catch(std::filesystem::filesystem_error &e){
-     std::cout<<"File "<<filename<<" do not exist or following error has occured"<<e.what()<<std::endl;
-   }
+void add(const std::string& filename) {
+    if (!std::filesystem::exists(STAGING_DIR)) {
+        std::filesystem::create_directory(STAGING_DIR);
+    }
+    if (!std::filesystem::exists(BLOBS_DIR)) {
+        std::filesystem::create_directory(BLOBS_DIR);
+    }
 
-   return;
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cerr << "File " << filename << " does not exist or cannot be opened!" << std::endl;
+        return;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string file_content = buffer.str();
+
+    std::string file_hash = calculate_sha1_hash(file_content);
+    std::string blob_path = BLOBS_DIR + "/" + file_hash;
+
+    if (!std::filesystem::exists(blob_path)) {
+        std::ofstream blob_file(blob_path, std::ios::binary);
+        blob_file << file_content;
+        blob_file.close();
+        std::cout << "Blob created for file: " << filename << " with hash: " << file_hash << std::endl;
+    }
+
+    std::ofstream index_file(INDEX_FILE, std::ios::app);
+    if (!index_file) {
+        std::cerr << "Error opening index file in the staging area!" << std::endl;
+        return;
+    }
+
+    index_file << file_hash << " " << filename << std::endl;
+    std::cout << "File " << filename << " is staged with blob hash: " << file_hash << std::endl;
+
+    index_file.close();
+    file.close();
 }
