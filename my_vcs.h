7a4b93de7bf23df6d863e6e9d42374570b3823b2
Dhruv0427 <b23ee1086@iitj.ac.in>
2024-10-14 15:47:20 +0530

    Update my_vcs.h to add functions of branch

--- my_vcs.h
+++ my_vcs.h
@@ -2,17 +2,32 @@
 #define MY_VCS_H
 
 #include<string>
+#include <unordered_map>
+#include<vector>
+#include<filesystem>
+
 
 void init();
+std::string calculate_sha1_hash(const std::string& content);
 void add(const std::string& filename);
-std::string generate_commit_hash(const std::vector<std::string>& file_hashes, const std::string& message, const std::string& prev_commit);
-std::string calculate_sha1_hash(const std::string& input);
-void commit(const std::string& message);
 void log();
 
-void restore_file(const std::string& filename);
-void restore_file_version(const std::string& filename, std::size_t version_number);
+std::string generate_commit_hash(const std::vector<std::string>& file_hashes, const std::string& message, const std::string& prev_commit);
+std::string create_blob(const std::filesystem::path& file_path);
+std::string create_tree(const std::vector<std::pair<std::string, std::string>>& file_entries);
+void commit(const std::string& message);
+
+std::string get_commit_tree_hash(const std::string& commit_hash);
+std::unordered_map<std::string, std::string> read_tree(const std::string& tree_hash);
 void list_file_versions(const std::string& filename);
-std::pair<std::string, std::string> split_versioned_filename(const std::string& versioned_file);
+void restore_file_version(const std::string& commit_hash, const std::string& filename);
+void restore_file(const std::string& filename);
+
+void list_branches();
+void create_branch(const std::string& branch_name);
+void switch_branch(const std::string& branch_name);
+void delete_branch(const std::string& branch_name);
+void handle_branch_command(const std::string& command, const std::string& branch_name);
+
 
 #endif
