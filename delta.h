4851fb9ceed9813c1172158a329237d58bad66c8
Dhruv0427 <b23ee1086@iitj.ac.in>
2024-10-21 13:45:42 +0530

    Create delta.h

---
+++ delta.h
@@ -0,0 +1,9 @@
+#ifndef DELTA_H
+#define DELTA_H
+
+#include <vector>
+#include <string>
+
+void generateUniqueDelta(const std::vector<std::string>& oldLines, const std::vector<std::string>& newLines);
+
+#endif
