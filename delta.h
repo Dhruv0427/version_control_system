f54223b06b1db2079d7c4fa71d27a54a83b60b2a
Dhruv0427 <b23ee1086@iitj.ac.in>
2024-10-25 16:43:16 +0530

    Update delta.h

--- delta.h
+++ delta.h
@@ -1,9 +1,20 @@
 #ifndef DELTA_H
 #define DELTA_H
 
-#include <vector>
 #include <string>
+#include <vector>
+#include <utility>
+#include <filesystem>
 
-void generateUniqueDelta(const std::vector<std::string>& oldLines, const std::vector<std::string>& newLines);
+namespace fs = std::filesystem;
+
+void storeDelta(const std::string &fileName,  
+                const std::vector<std::string> &added, 
+                const std::vector<std::pair<int, std::pair<std::string, std::string>>> &modified,  
+                const std::vector<std::string> &deleted);
+
+void displayDelta(const std::string &deltaFile);
+
+std::string getNextDeltaFileName(const std::string &fileName);
 
 #endif
