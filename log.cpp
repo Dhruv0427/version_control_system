582143713fd1c4e49736d4d71efbb16a37785794
Dhruv0427 <b23ee1086@iitj.ac.in>
2024-11-04 07:47:50 +0530

    Update log.cpp

--- log.cpp
+++ log.cpp
@@ -1,10 +1,9 @@
- 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include "my_vcs.h"
 
-const std::string METADATA_FILE = ".vcs/commits/metadata.txt";
+const std::string METADATA_FILE = ".vcs/METADATA";
 
 void log() {
     std::ifstream metadata(METADATA_FILE);
@@ -17,8 +16,9 @@ void log() {
     std::string line;
     std::cout << "Commit History:\n";
     while (std::getline(metadata, line)) {
-        std::cout << line << std::endl;
+        std::cout << line << std::endl; // Output each line from the metadata file
     }
 
     metadata.close();
 }
+
