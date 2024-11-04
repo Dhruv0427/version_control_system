c1dca9d791b2cd54005e7d6ff687e8b62492a247
Dhruv0427 <b23ee1086@iitj.ac.in>
2024-11-04 07:47:23 +0530

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -8,19 +8,9 @@ void init() {
     if (!std::filesystem::exists(".vcs")) {
         // Create the .vcs directory and its subdirectories
         std::filesystem::create_directory(".vcs");
-        std::filesystem::create_directory(".vcs/commits");
-        std::filesystem::create_directory(".vcs/staging");
-        std::filesystem::create_directory(".vcs/blobs");
-        std::filesystem::create_directory(".vcs/trees");
-
-        // Create the HEAD file and point it to the master branch
-        std::ofstream head_file(".vcs/HEAD");
-        head_file << "master";  // Set default branch to master
-        head_file.close();
 
         std::cout << "Directory structure '.vcs' created in the directory: " 
                   << std::filesystem::current_path() << std::endl;
-        std::cout << "HEAD file created, pointing to the default branch 'master'." << std::endl;
         std::cout << "---------------------------------------------------------------------------------------------------" << std::endl;
     } else {
         std::cout << "'.vcs' directory already exists." << std::endl;
