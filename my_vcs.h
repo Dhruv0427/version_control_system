ca39154dc93449bd6818ee20caebaecf1942c5be
Dhruv0427 <b23ee1086@iitj.ac.in>
2024-11-04 07:50:01 +0530

    Update my_vcs.h

--- my_vcs.h
+++ my_vcs.h
@@ -1,33 +1,23 @@
 #ifndef MY_VCS_H
 #define MY_VCS_H
 
-#include<string>
+#include <string>
 #include <unordered_map>
-#include<vector>
-#include<filesystem>
+#include <vector>
+#include <filesystem>
 
-
+// Function declarations
 void init();
-std::string calculate_sha1_hash(const std::string& content);
 void add(const std::string& filename);
 void log();
-
-std::string generate_commit_hash(const std::vector<std::string>& file_hashes, const std::string& message, const std::string& prev_commit);
-std::string create_blob(const std::filesystem::path& file_path);
-std::string create_tree(const std::vector<std::pair<std::string, std::string>>& file_entries);
-void commit(const std::string& message);
-
-std::string get_commit_tree_hash(const std::string& commit_hash);
-std::unordered_map<std::string, std::string> read_tree(const std::string& tree_hash);
-void list_file_versions(const std::string& filename);
-void restore_file_version(const std::string& commit_hash, const std::string& filename);
-void restore_file(const std::string& filename);
-
-void list_branches();
-void create_branch(const std::string& branch_name);
-void switch_branch(const std::string& branch_name);
-void delete_branch(const std::string& branch_name);
-void handle_branch_command(const std::string& command, const std::string& branch_name);
-
-
-#endif
+void diff(std::string fileName);
+void compareFiles(const std::string& currentFile, const std::string& prevFile); 
+std::string readFile(const std::string& filePath);
+void storeDelta(const std::string& currentFile, 
+                const std::vector<std::string>& added, 
+                const std::vector<std::pair<int, std::pair<std::string, std::string>>>& modified,  
+                const std::vector<std::string>& deleted);
+void displayDelta(const std::string &deltaFile);
+std::string getNextDeltaFileName(const std::string &fileName);
+void commit(std::string message);
+#endif // MY_VCS_H
